3010f4cafb155be1d17cd0ca17f6459831290784
Rezvanov Roman <rrrezzz@mail.ru>
2020-03-17 18:04:24 +0300

    add move

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -36,12 +36,26 @@ namespace Snake
             {
                 x = x - offset;
             }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
         }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
         }
 
         public override string ToString()
