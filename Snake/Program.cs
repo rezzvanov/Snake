c9955731fb73eaf538773661681e1b921e47b4a8
Rezvanov Roman <rrrezzz@mail.ru>
2020-03-17 11:06:34 +0300

    add List

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -18,6 +18,26 @@ namespace Snake
 
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[0];
+            int z = numList[0];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
