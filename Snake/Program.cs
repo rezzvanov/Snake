fbbb8fe708b603fd4ad21d6d64cf3eed697992f1
Rezvanov Roman <rrrezzz@mail.ru>
2020-03-17 18:38:57 +0300

    add eat

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,7 +15,7 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0 , '+');
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
@@ -27,10 +27,24 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            
-            while(true)
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
             {
-                if(Console.KeyAvailable)
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
