11969381a042f1eff2a734f51e298917fba39cb1
Rezvanov Roman <rrrezzz@mail.ru>
2020-03-17 19:00:21 +0300

    polymorphism

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,47 +11,74 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(1, 1);
-            Console.SetBufferSize(80, 25);
-            Console.SetWindowSize(80, 25);
-
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downline.Draw();
-            leftline.Draw();
-            rightline.Draw();
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            Draw(v1);
 
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
 
-            while (true)
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach(var f in figures)
             {
-                if(snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
-                Thread.Sleep(100);
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
-                Thread.Sleep(100);
-                snake.Move();
+                f.Draw();
             }
+
+            Console.ReadKey();
+
+            //Console.SetWindowSize(1, 1);
+            //Console.SetBufferSize(80, 25);
+            //Console.SetWindowSize(80, 25);
+
+            //HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            //HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            //VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            //VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            //upLine.Draw();
+            //downline.Draw();
+            //leftline.Draw();
+            //rightline.Draw();
+
+            //Point p = new Point(4, 5, '*');
+            //Snake snake = new Snake(p, 4, Direction.RIGHT);
+            //snake.Draw();
+
+            //FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            //Point food = foodCreator.CreateFood();
+            //food.Draw();
+
+            //while (true)
+            //{
+            //    if(snake.Eat(food))
+            //    {
+            //        food = foodCreator.CreateFood();
+            //        food.Draw();
+            //    }
+            //    else
+            //    {
+            //        snake.Move();
+            //    }
+            //    Thread.Sleep(100);
+            //    if (Console.KeyAvailable)
+            //    {
+            //        ConsoleKeyInfo key = Console.ReadKey();
+            //        snake.HandleKey(key.Key);
+            //    }
+            //    Thread.Sleep(100);
+            //    snake.Move();
+            //}
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
         }
     }
 }
